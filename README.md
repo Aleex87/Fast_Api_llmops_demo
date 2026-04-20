ee2b57b67f80ba631b1048aacd5d430d5215e970
Aleex87 <alessandrodanteabbate@gmail.com>
2026-04-20 10:16:13 +0200

    build tha agent and a get point for test

--- README.md
+++
@@ -1,1 +0,0 @@
-# Fast_Api_llmops_demo
---
+++ customer_support/src/customer_support/backend/agents.py
@@ -0,0 +1,24 @@
+from pydantic_ai import Agent
+from constants import MODEL_MEDIUM
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# NOTE: change system prompt to load from MLFlow later
+support_agent = Agent(
+    model=MODEL_MEDIUM, system_prompt="You are a customer support agent"
+)
+
+@support_agent.tool_plain
+def lookup_faq(category: str) -> str:
+    """FAQ for customer support"""
+    faq = {
+        "refund": "Full refunds within 30 days with receipt.",
+        "shipping": "Standard: 5-7 days. Express available.",
+        "warranty": "1-year manufacturer warranty included.",
+    }
+    return faq.get(
+        category,
+          "Question not found. Inform customer that available categoris are: refound , shipping, warranty",
+                   )
+
