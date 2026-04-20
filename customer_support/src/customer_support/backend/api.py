c0f1fa5dca9c652dc4e205785d9e048daa18e19b
Aleex87 <alessandrodanteabbate@gmail.com>
2026-04-20 13:32:36 +0200

    build a evaluation dataset

--- customer_support/src/customer_support/backend/api.py
+++ customer_support/src/customer_support/backend/api.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
 from customer_support.backend.agents import support_agent
+from customer_support.backend.middleware import logging_middleware
+
 
 app = FastAPI()
+logging_middleware(app=app)
+
 
 # get for simplicity -> in real cases use post
 @app.get("/customer_support")
 async def customer_support_faq(question: str) -> str:
     result = await support_agent.run(question)
-    return result.output 
+    return result.output
--- customer_support/src/customer_support/backend/constants.py
+++ customer_support/src/customer_support/backend/constants.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import mlflow
 
+
 MODEL_SMALL="openrouter:liquid/lfm-2.5-1.2b-instruct:free"
 MODEL_MEDIUM="openrouter:nvidia/nemotron-3-nano-30b-a3b:free"
 MODEL_LARGE="openrouter:nvidia/nemotron-3-super-120b-a12b:free"
@@ -9,7 +10,7 @@ LLM_JUDGE="openrouter:/nvidia/nemotron-3-nano-30b-a3b:free"
 
 MONITORING_PATH = Path(__file__).parents[1] / "monitoring"
 
-# when constans modul is imported this will be run  -> we use mlflof.db inside of monitori
+# when constants module is imported -> this will run -> we use mlflow.db inside of monitoring
 mlflow.set_tracking_uri(f"sqlite:///{MONITORING_PATH / 'mlflow.db'}")
-
-
+mlflow.set_experiment("customer_support_bot")
+mlflow.pydantic_ai.autolog()
